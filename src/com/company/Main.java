6516b108be6d253e4c2d1f78d0895775872a5dd7
Abdugafur111 <64993553+Abdugafur111@users.noreply.github.com>
2021-04-06 13:54:39 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final double PI = 3.14;
+
+        System.out.println(PI);
+
+
+
+    }
+}
